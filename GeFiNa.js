5a4650ae38518619e13ea86e4e927318338756ea
matejlajevec <matej.lajevec@gmail.com>
2020-11-29 22:06:05 +0100

    Sprememba načina vnosa odgovorov

--- GeFiNa.js
+++ GeFiNa.js
@@ -1,43 +1,36 @@
-var pos = 0, test, test_status, vprasanje, izbira, izbire, izbA, izbB, izbC, pravilno = 0;
+var pos = 0, test, test_status, vprasanje, pravilno = 0;
+var odgovori = []; 
 var vprasanja = [
-    [ "What is 10 + 4?", "12", "14", "16", "B" ],
-	[ "What is 20 - 9?", "7", "13", "11", "C" ],
-	[ "What is 7 x 3?", "21", "24", "25", "A" ],
-	[ "What is 8 / 2?", "10", "2", "4", "C" ]
+    [ "What is 10 + 4?", 14],
+	[ "What is 20 - 9?", 11],
+	[ "What is 7 x 3?", 21],
+	[ "What is 8 / 2?", 4]
 ];
+
 function _(x){
 	return document.getElementById(x);
 }
+
 function prikazVprasanja(){
 	test = _("test");
 	if(pos >= vprasanja.length){
-		test.innerHTML = "<h2>Dobil si "+pravilno+" od "+vprasanja.length+" pravilna vprašanja</h2>";
+		test.innerHTML = "<h2>Dobil si " + (pravilno/vprasanja.length)*100 + "%</h2>";
 		_("test_status").innerHTML = "Test končan";
-		pos = 0;
-		pravilno = 0;
-		return false;
+		return ;
 	}
-	_("test_status").innerHTML = "Vprašanje "+(pos+1)+" od "+vprasanja.length;
+	_("test_status").innerHTML = "Vprašanje "+ (pos+1) +" od " + vprasanja.length;
 	vprasanje = vprasanja[pos][0];
-	izbA = vprasanja[pos][1];
-	izbB = vprasanja[pos][2];
-	izbC = vprasanja[pos][3];
-	test.innerHTML = "<h3>"+vprasanje+"</h3>";
-	test.innerHTML += "<input type='radio' name='izbire' value='A'> "+izbA+"<br>";
-	test.innerHTML += "<input type='radio' name='izbire' value='B'> "+izbB+"<br>";
-	test.innerHTML += "<input type='radio' name='izbire' value='C'> "+izbC+"<br><br>";
+	test.innerHTML = "<h3>"+vprasanje+"</h3>";	
+	test.innerHTML += "<input type = 'num' id = 'vnos' >";
 	test.innerHTML += "<button onclick='preveriOdgovor()'>Potrdi izbiro</button>";
 }
+
 function preveriOdgovor(){
-	izbire = document.getElementsByName("izbire");
-	for(var i=0; i<izbire.length; i++){
-		if(izbire[i].checked){
-			izbira = izbire[i].value;
-		}
-	}
-	if(izbira == vprasanja[pos][4]){
+	var vnos = document.getElementById("vnos").value;
+	if(vnos == vprasanja[pos][1]){
 		pravilno++;
 	}
+	odgovori.push(vnos);
 	pos++;
 	prikazVprasanja();
 }
